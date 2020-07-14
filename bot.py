51a7f440874766d9df1a0af799fbee052a8f9d86
Braden Wong <bmw02002turbo@gmail.com>
2020-07-13 19:00:17 -0700

    Uploaded Selenium Bot Detects presence of element (Green Light) and clicks once recognized.

---
+++ bot.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox()
+driver.get("https://humanbenchmark.com/tests/reactiontime")
+box = driver.find_element_by_xpath("/html/body/div/div/div[4]/div[1]")
+# box.click()
+
+try:
+    while True:
+        newbox = WebDriverWait(driver, 300, poll_frequency=0.0000000000001).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "view-go"))
+        )
+        newbox.click()
+
+finally:
+    driver.quit()
