5991c2253296622950c2d815d69a1b7270f5fa15
Braden Wong <bmw02002turbo@gmail.com>
2020-07-15 12:28:00 -0700

    Added comments

--- human-benchmark-reaction-test-bot.py
+++ human-benchmark-reaction-test-bot.py
@@ -5,11 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://humanbenchmark.com/tests/reactiontime")
-box = driver.find_element_by_xpath("/html/body/div/div/div[4]/div[1]")
+
+# If you wish to autoclick the start button, uncomment the code below
+# box = driver.find_element_by_xpath("/html/body/div/div/div[4]/div[1]")
 # box.click()
 
 try:
     while True:
+        # Waits until green box is detected, then clicks
         newbox = WebDriverWait(driver, 300, poll_frequency=0.0000000000001).until(
             EC.presence_of_element_located((By.CLASS_NAME, "view-go"))
         )
